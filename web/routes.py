af8e95ea62d2047f9d9917cfd3c072c75f6a6cf8
salamanderman234 <tresnasaputra9@gmail.com>
2023-05-09 14:53:10 +0800

    feat: menambahkan fitur lihat semua terms

--- web/routes.py
+++ web/routes.py
@@ -13,15 +13,27 @@ vsm_tool = VSM.VSMTool()
 def index():
     return render_template("index.html")
 
+@app.route("/terms")
+def terms():
+    documents = get_all_documents(is_get_dist=True)
+    terms = {}
+    for document in documents:
+        for term in document["term_dist"]:
+            if term["term"] in terms :
+                terms[term["term"]] += term["freq"]
+            else:
+                terms[term["term"]] = term["freq"]
+    
+    return render_template("terms.html", title="All terms", terms=terms)
+
 @app.route("/documents")
 def show_all_documents():
-    documents = Document.query.all()
-    documents = deconstructor.bulk_deconstruct(documents)
-    return render_template("documents.html", documents=documents)
+    documents = get_all_documents()
+    return render_template("documents.html", title="All documents" ,documents=documents)
 
 @app.route("/documents/add")
 def add():
-    return render_template("add.html")
+    return render_template("add.html", title="Add new document")
 
 @app.route("/documents/save", methods=["POST"])
 def save():
@@ -36,14 +48,21 @@ def save():
 @app.route("/result")
 def result():
     query = request.args.get("q")
-    documents = Document.query.all()
-    documents = deconstructor.bulk_deconstruct(documents)
-    for index, document in enumerate(documents):
-        documents[index]["term_dist"] = preprocessor.process(document["content"])
+    documents = get_all_documents(is_get_dist=True)
     
     tfidf_result = search_using_tfidf(query=query, documents=documents)
 
-    return render_template("result.html", documents=tfidf_result)
+    return render_template("result.html", title="Hasil pencarian" ,documents=tfidf_result)
+
+def get_all_documents(is_get_dist=False) -> list :
+    documents = Document.query.all()
+    documents = deconstructor.bulk_deconstruct(documents)
+    if is_get_dist:
+        for index, document in enumerate(documents):
+            documents[index]["term_dist"] = preprocessor.process(document["content"])
+            print("asiap")
+
+    return documents
 
 def search_using_tfidf(query: str, documents: list) -> list:
     query_terms = preprocessor.process(query)
